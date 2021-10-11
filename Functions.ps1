7a1cb4464747dffcaaa5e06d72bdacd8e1215f3b
JDQue <60594247+jkrieger12@users.noreply.github.com>
2021-10-11 10:38:55 -0700

    Update Functions.ps1

--- Functions.ps1
+++ Functions.ps1
@@ -13,4 +13,8 @@ function Get-ComputerDiskUsage{
   #Get disk information
   $CDrive = Get-WMIObject -Class win32_logicaldisk -ComputerName $ComputerName | where DeviceID -eq 'C:'
   $FreeSpace = [math]::Round(($CDrive.FreeSpace/1073741824), 2)
+  $DiskUsage = 100 - [math]::Round(($CDrive.Freespace / $CDrive.Size)*100,2)
+  
+  Write-Host "Freespace = $($Freespace) GB"
+  Write-Host "Diskusage = $($DiskUsage) %"
 }
